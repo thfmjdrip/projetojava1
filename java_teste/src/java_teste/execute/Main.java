dfbe98a5973093d978dedbb45adbdd02276ef4a5
thfmjdrip <thiagodrip@gmail.com>
2024-06-13 16:28:56 -0300

    Ultimo commit

--- java_teste/src/java_teste/execute/Main.java
+++ java_teste/src/java_teste/execute/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("teste......");
 		System.out.println("teste1......");
+		System.out.println("Ultimo......");
 	}
 
 }
